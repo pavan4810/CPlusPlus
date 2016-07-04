433d8350b3a7c983890acdb3ecb9b16840f33a38
Pavan Kumar Avala <pavan4810@gmail.com>
2016-07-04 13:03:27 -0400

    003 : Variable definition

---
+++ 003_var_def.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main(void)
+{
+  int i;
+  int j=5;
+  printf("i is %i j is %i \n",i,j);
+
+
+
+  for ( i = 0; i < 3; i++ )
+  {
+    int i = 0;                          // in a new scope, a new i is created
+    printf("INSIDE LOOP i is %i\n",i);  // this is the inside i
+  }
+  printf("After loop i is %i\n",i);
+
+
+
+  for ( int l = 0; l < 3; l++ )         // l is created once, at start of loop
+  {
+    int l = 0;                          // in a new scope, a new l is created
+    printf("inside loop l is %i \n",l); // this is the inside l
+  }                                     // the inside l goes out of scope and
+                                        // is deallocated each time around loop
+  printf("After loop l is %i\n", l);
+
+
+#ifdef BUG
+  // this would be a redefinition of l, remember l is local to main
+  // l is not local to the loop, many programmers confuse this issue
+
+  for ( int l = 0; l < 3; l++ )         // l is created once, at start of loop
+  {
+    int l = 0;                          // in a new scope, a new l is created
+    printf("inside loop l is %i \n",l); // this is the inside l
+  }                                     // the inside l goes out of scope and
+                                        // is deallocated each time around loop
+  printf("After loop l is %i\n",l);
+#endif
+}
